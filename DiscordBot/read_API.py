9e710fd8b0ee69abad1d1592d0057f8b5fdc93fa
MikolajMagaj <322990@uwr.edu.pl>
2020-11-14 18:54:16 +0100

    poprawiony task

--- DiscordBot/read_API.py
+++ DiscordBot/read_API.py
@@ -4,16 +4,14 @@ def read_all_files():#wczyta wszystkie pliki
     paths = Path('API_data').glob('*.*')
     for path in paths:
         path_in_str = str(path)#konwersja z objektu na string
-        f=open(path_in_str,"r")
-        print(f.read())
-        f.close()
+        with open(path_in_str) as f:
+            print(f.read())
     return True
-#read_all_files()
+read_all_files()
 
 def read_file(file):#wczyta podany plik
-    f=open("API_data/"+file,"r")
-    print(f.read())
-    f.close()
+    with open("API_data/"+file) as f:
+        print(f.read())
     return True
 read_file("test.txt")
 
